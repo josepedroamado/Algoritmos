20c355aae11d7b1a24db921c5898d72fc60384b1
José Pedro Amado <jose.pedro.amado@outlook.com>
2017-12-02 17:16:44 -0300

    - Implementación ParentesisBalanceados e ImprimirPorNiveles - Fin del trabajo

--- Entrega2.cpp
+++ Entrega2.cpp
@@ -5,13 +5,54 @@
 
 bool ParentesisBalanceados(char *formula)
 {
-	// NO IMPLEMENTADA
-	return false;
+	//IMPLEMENTADA
+	int ret = true;
+	Pila<char>* pila = new PilaImp<char>();
+	int i = 0;
+	while (formula[i] != '\0'){
+		if (formula[i] == '(' || formula[i] == '[' || formula[i] == '{') pila->Push(formula[i]);
+		if (formula[i] == ')') {
+			if (pila->EsVacia()) pila->Push(formula[i]);
+			else if (pila->Top() == '(') pila->Pop();
+		}
+		if (formula[i] == ']') {
+			if (pila->EsVacia()) pila->Push(formula[i]);
+			else if (pila->Top() == '[') pila->Pop();
+		}
+		if (formula[i] == '}') {
+			if (pila->EsVacia()) pila->Push(formula[i]);
+			else if (pila->Top() == '{') pila->Pop();
+		}
+		i++;
+	}
+	ret = pila->EsVacia();
+	delete pila;
+	return ret;
 }
 
 void ImprimirPorNiveles(NodoAG* a)
 {
-	// NO IMPLEMENTADA
+	//IMPLEMENTADA
+	if (a != NULL) {
+		Cola<NodoAG*>* colaNivel = new ColaImp<NodoAG*>();
+		Pila<NodoAG*>* pilaHermanos = new PilaImp<NodoAG*>();
+		colaNivel->Encolar(a);
+		while (!colaNivel->EsVacia()){
+			NodoAG* nodo = colaNivel->Desencolar();
+			if (nodo->ph != NULL) colaNivel->Encolar(nodo->ph);
+			if (nodo->sh != NULL) pilaHermanos->Push(nodo->sh);
+			cout << nodo->dato;
+			while (!pilaHermanos->EsVacia()){
+				nodo = pilaHermanos->Pop();
+				if (nodo->ph != NULL) colaNivel->Encolar(nodo->ph);
+				if (nodo->sh != NULL) pilaHermanos->Push(nodo->sh);
+				cout << ' ' << nodo->dato;
+			}
+			if (!colaNivel->EsVacia()) cout << ' ';
+		}
+		delete colaNivel;
+		delete pilaHermanos;
+	}
 }
 
 #endif
